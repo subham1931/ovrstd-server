fdcdd08b57171dc65108dfd47528fb0e398b88f6
Nobisuke19 <subham019650@gmail.com>
2026-03-22 11:56:52 +0530

    app-package

--- app.js
+++ app.js
@@ -4,25 +4,31 @@ import path from "path";
 import { fileURLToPath } from "url";
 import connectDB from "./db/connection.js";
 import cors from "cors";
+import userRoutes from "./routes/userRoute.js";
 
-// For __dirname in ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+
 app.use(cors());
-
 app.use(express.json());
-// Serve the uploads folder statically
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-connectDB();
-app.get("/", (req, res) => {
-    res.send("Server is running");
-})
 
-import userRoutes from "./routes/userRoute.js";
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ success: true, message: "API is healthy" });
+});
+
 app.use("/auth", userRoutes);
 
-app.listen(3000,() => {
-    console.log("Server is running on port 3000")
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
