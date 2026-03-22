9ebeb8e26c6c55692bfd2933b5070038c3cb8788
Nobisuke19 <subham019650@gmail.com>
2026-03-22 11:37:53 +0530

    / route

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import connectDB from "./db/connection.js";
+import cors from "cors";
 
 // For __dirname in ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 // Serve the uploads folder statically
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 connectDB();
+app.get("/", (req, res) => {
+    res.send("Server is running");
+})
 
 import userRoutes from "./routes/userRoute.js";
 app.use("/auth", userRoutes);
